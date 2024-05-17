8c5148ae642ee77cab712d8072cb2e1bfd36a4e0
Izaqui <izaquiel.silva.ads@gmail.com>
2024-05-17 12:50:25 -0300

    iniciando projeto com spring boot

---
+++ project_dev/src/main/java/com/example/project_dev/ProjectDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDevApplication.class, args);
+	}
+
+}
